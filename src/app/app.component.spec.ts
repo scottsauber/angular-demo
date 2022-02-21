dfe434ec4d5e34c88b2d8a6066b8ab811d82fca0
Scott Sauber <stsauber@gmail.com>
2022-02-20 22:24:03 -0600

    convert from karma to jest/atl

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { render, RenderResult, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: RenderResult<AppComponent>;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-demo');
+    fixture = await render(AppComponent);
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-demo app is running!');
+    const message = screen.getByText('angular-demo app is running!');
+    expect(message).toBeInTheDocument();
   });
 });
